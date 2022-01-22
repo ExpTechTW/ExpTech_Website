73e671532f2a880409fe93d68187a091f54c4034
smile-minecraft <54743433+smile-minecraft@users.noreply.github.com>
2022-01-22 17:55:03 +0800

    增加index.php的修改 加入css

--- server/index.php
+++ server/index.php
@@ -11,9 +11,11 @@ $FormatVersion=1;
     session_start();
     if (!empty($_SESSION['user'])) {
         $user=$_SESSION['user'];
-        echo '<p><a href="?action=logout">登出</a></p>';
+        echo '<div class="topcorner"><div class="box"><button type="button" class="standard" onclick="location.href='?action=logout'">登出</button></div></div>';
+        echo '<div class="topcorner-2"><div class="box"><button type="button" class="standard" onclick="location.href='?action=block'">方塊數據</button></div></div>';
+        echo '<div class="topcorner-3"><div class="box"><button type="button" class="standard" onclick="location.href='?action=economy'">經濟玩法</button></div></div>';
     }else{
-        echo '<p><a href="?action=login">登入</a></p>';
+        echo '<div class="topcorner"><div class="box"><button type="button" class="standard" onclick="location.href='?action=login'">登入</button></div></div>';
     }
 
     if(get('action') == 'login') {
@@ -23,6 +25,14 @@ $FormatVersion=1;
     if(get('action') == 'logout') {
         session_destroy();
         header('Location: index.php');
+    }
+
+    if(get('action') == 'block') {
+        header('Location: block.php');
+    }
+
+    if(get('action') == 'economy') {
+        header('Location: economy.php');
     }
 
     function get($key, $default=NULL) {
@@ -35,8 +45,49 @@ $FormatVersion=1;
 <head>
     <meta charset="utf-8">
     <title>ExpTech.tw | 探索科技</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="images/head.ico">
+    <style>
+        .standard{
+            color:white;
+            background-color:hsl(193, 58%, 52%) ;
+            padding: 10px;
+            font-family: sans-serif;
+            margin: 50px ;
+            font-size: larger;
+            background-image:url('../../Downloads/homebg.png') ;
+            border-radius: 10px;
+        }
+        .container{
+            display: flex;
+            justify-content: center; 
+            align-items: center; 
+        }
+        .topcorner{
+            position:absolute;
+            top:0;
+            right:0;
+        }
+        .topcorner-2{
+            position:absolute;
+            top:0;
+            right: 80px;
+        }
+        .topcorner-3{
+            position:absolute;
+            top:0;
+            right:200px;
+        }
+    </style>
 </head>
-<body>
+<body bgcolor="#b0deeb">
+    <br>
+    <br>
+    <br>
+    <div class="standard">
+        <h1>探索科技<br>Exptech</h1>
+    </div>
 <?php
 if(empty($user)) return;
 echo '<table class="table table-bordered table-striped table-condensed"><tr><td>名稱</td><td>數量</td></tr>';
