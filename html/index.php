93dbf8aad0b24cc7cac7419b969fedefad0a0f2f
vincentwang0905(炸蝦好吃) <VincentX0905@gmail.com>
2022-02-01 23:08:04 +0800

    整併CSS與JS，增加測試版本說明框架

--- html/index.php
+++ html/index.php
@@ -1,277 +1,60 @@
+<html>
+<head>
+<title>ExpTech.tw | 探索科技</title>
+<meta charset="utf-8"/>
+<meta name="description" content="">
+<meta name="author" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta content="數據網站,登入可以查看數據!" name="description">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta content="exptech,探索科技,探索科技數據,exptech.tw,Exptech,Exptech.TW" name="keywords">
+<meta property="og:locale" content="zh_TW" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="ExpTech.TW-數據網站" />
+<meta property="og:description" content="數據網站,登入可以查看數據!" />
+<meta property="og:url" content="https://exptechweb.mywire.org/" />
+<meta property="og:site_name" content="ExpTech.TW" />
+<meta property="og:image" content="https://raw.githubusercontent.com/ExpTechTW/ExpTech_Website/%E4%B8%BB%E8%A6%81%E7%9A%84-(main)/html/images/head.ico" />
+<meta property="og:image:width" content="500" /><meta property="og:image:height" content="500" />
+<meta property="og:image:type" content="image/x-icon" />
+<link rel="icon" type="image/png" href="images/head.ico">
+<script src="../js/all.js"></script>
+<script src="../js/brands.js"></script>
+<script src="../js/solid.js"></script>
+<script src="../js/fontawesome.js"></script>
+<script src="../js/main.js"></script>
+<link href="../css/main.css"rel="stylesheet">
+<link href="../css/all.css" rel="stylesheet">
+<link href="../css/fontawesome.css" rel="stylesheet">
+<link href="../css/brands.css" rel="stylesheet">
+<link href="../css/solid.css" rel="stylesheet">
+</head>
+<body class="banner" oncontextmenu="return false" onselectstart="return false" ondragstart="return false" onbeforecopy="return false"  onmousewheel="return false;" onmouseup=document.selection.empty() oncopy=document.selection.empty() onselect=document.selection.empty()>
 <?php
 
 $Decoder=null;
-$Language="zh-Hant-TW"; //en-US
+$Language="zh-TW"; //en-US
 
     if  (!empty($_COOKIE['user'])) {
-        echo '<style>
-.button {
-  display: inline-block;
-  padding: 10px 10px;
-  font-size: 22px;
-  width: 180px;
-  cursor: pointer;
-  text-align: center;
-  text-decoration: none;
-  transition: all 0.3s;
-  outline: none;
-  color: #fff;
-  justify-content: center;
-  background-color: #2894FF;
-  border: none;
-  height: 2.2em;
-  border-radius: 16px;
-  box-shadow: 0 5px #999;
-  margin: 2px;
-}
-.button:hover {background-color: #0066cc}
-
-.button:active {
-  background-color: #0066cc;
-  box-shadow: 0 2px #666;
-  transform: translateY(4px);
-}
-.button span {
-  cursor: pointer;
-  display: inline-block;
-  position: relative;
-  transition: 0.3s;
-}
-
-.button span:after {
-  content: "\00bb";
-  position: absolute;
-  opacity: 0;
-  top: 0;
-  right: -10px;
-  color: #FFFFFF;
-  transition: 0.3s;
-}
-
-.button:hover span {
-  padding-right: 25px;
-}
-
-.button:hover span:after {
-  opacity: 1;
-  right: 0;
-}
-.ahref {
-  text-decoration: none;
-  color: #FFFFFF;
-}
-.banner {
-  background-attachment: fixed;
-  background-image: linear-gradient(rgb(145, 232, 255), rgb(255, 255, 255));
-  background-position: center;
-}
-.blink {
-    color: #C0C0C0;
-    font-size: 20px;
-    font-weight: bold;
-    font-family: sans-serif;
-}
-.logo {
-		display: inline-block;
-		height: inherit;
-		left: 1.5em;
-		line-height: inherit;
-		margin: 0;
-		padding: 0;
-    position: absolute;
-		top: 0;
-}
-@media (max-width: 800px){
-							.button{
-                font-size: 14px;
-                width: 100px;
-                height: 3.0em;
-							}
-              .logo {
-                font-size: 14px;
-							}
-              .button:hover span {
-                padding-right: 8%;
-              }
-}
-.header {
-    background: #272727;
-    color: #1c1d26;
-    height: 3.7em;
-    left: 0;
-    line-height: 3.7em;
-    position: fixed;
-    text-align: right;
-    top: 0;
-    width: 100%;
-    z-index: 10001;
-}
-</style>
-<html>
-<head>
-<title>ExpTech.tw | 探索科技</title>
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width">.
-<meta content="ExpTech.tw | 探索科技" property="og:title" />
-<meta content="數據網站,登入可以查看數據!" property="og:description" />
-<meta content="https://exptechweb.mywire.org/" property="og:url" />
-<meta content="../images/head.ico" property="og:image" />
-<meta content="#91e8ff" data-react-helmet="true" name="theme-color" />
-<script defer src="../js/all.js"></script>
-<script defer src="../js/brands.js"></script>
-<script defer src="../js/solid.js"></script>
-<script defer src="../js/navpanel.js"></script>
-<script defer src="../js/fontawesome.js"></script>
-<link href="../css/all.css" rel="stylesheet">
-<link href="../css/fontawesome.css" rel="stylesheet">
-<link href="../css/brands.css" rel="stylesheet">
-<link href="../css/solid.css" rel="stylesheet">
-</head>
-<body class="banner">
-<header class="header">
-<h3 class="logo blink">ExpTech.tw | 探索科技</h3>
+        echo '<header class="header">
+<h3 class="logo logotext">ExpTech.tw | 探索科技</h3>
 <a href="?action=block" class="ahref"><button class="button" style="vertical-align:middle"><span><i class="fas fa-cubes"></i>方塊數據</span></button></a><a href="?action=economy" class="ahref"><button class="button" style="vertical-align:middle"><span><i class="far fa-money-bill-alt"></i>經濟玩法</span></button></a><a href="?action=logout" class="ahref"><button class="button" style="vertical-align:middle"><span><i class="fas fa-sign-out-alt"></i>'.Decoder(2).'</span></button></a>
 </header>
-</body>
-</html>
-';
+<div class="loginfrom">
+<p class="ptext"><i class="fas fa-exclamation-triangle"></i>注意:</p>
+<p class="ptext">網站目前為測試版!</p>
+<p class="ptext">有問題請盡速回報!</p>
+</div>';
     }else{
-        echo '<style>
-.button {
-  display: inline-block;
-  padding: 5px 5px;
-  font-size: 22px;
-  width: 180px;
-  cursor: pointer;
-  text-align: center;
-  text-decoration: none;
-  transition: all 0.3s;
-  outline: none;
-  color: #fff;
-  justify-content: center;
-  background-color: #2894FF;
-  border: none;
-  height: 2.2em;
-  border-radius: 16px;
-  box-shadow: 0 5px #999;
-  margin: 2px;
-}
-.button:hover {background-color: #0066cc}
-
-.button:active {
-  background-color: #0066cc;
-  box-shadow: 0 2px #666;
-  transform: translateY(4px);
-}
-.button span {
-  cursor: pointer;
-  display: inline-block;
-  position: relative;
-  transition: 0.3s;
-}
-
-.button span:after {
-  content: "\00bb";
-  position: absolute;
-  opacity: 0;
-  top: 0;
-  right: -10px;
-  color: #FFFFFF;
-  transition: 0.3s;
-}
-
-.button:hover span {
-  padding-right: 25px;
-}
-
-.button:hover span:after {
-  opacity: 1;
-  right: 0;
-}
-.ahref {
-  text-decoration: none;
-  color: #FFFFFF;
-}
-.banner {
-  background-attachment: fixed;
-  background-image: linear-gradient(rgb(145, 232, 255), rgb(255, 255, 255));
-  background-position: center;
-}
-.blink {
-    color: #C0C0C0;
-    font-size: 20px;
-    font-weight: bold;
-    font-family: sans-serif;
-}
-.logo {
-		display: inline-block;
-		height: inherit;
-		left: 1.5em;
-		line-height: inherit;
-		margin: 0;
-		padding: 0;
-    position: absolute;
-		top: 0;
-}
-@media (max-width: 800px){
-							.button{
-                font-size: 14px;
-                width: 100px;
-                height: 3.3em;
-                z-index: 10002;
-							}
-              .logo {
-                font-size: 14px;
-							}
-              .button:hover span {
-                padding-right: 8%;
-              }
-}
-@media (max-width: 800px){
-              .header {
-                height: 3em;
-                line-height: 3em;
-              }
-}        
-.header {
-    background: #272727;
-    color: #1c1d26;
-    height: 3.7em;
-    left: 0;
-    line-height: 3.7em;
-    position: fixed;
-    text-align: right;
-    top: 0;
-    width: 100%;
-    z-index: 10001;
-}
-</style>
-<html>
-<head>
-<title>ExpTech.tw | 探索科技</title>
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width">.
-<meta content="ExpTech.tw | 探索科技" property="og:title" />
-<meta content="數據網站,登入可以查看數據!" property="og:description" />
-<meta content="https://exptechweb.mywire.org/" property="og:url" />
-<meta content="../images/head.ico" property="og:image" />
-<meta content="#91e8ff" data-react-helmet="true" name="theme-color" />
-<script defer src="../js/all.js"></script>
-<script defer src="../js/brands.js"></script>
-<script defer src="../js/solid.js"></script>
-<script defer src="../js/fontawesome.js"></script>
-<link href="../css/all.css" rel="stylesheet">
-<link href="../css/fontawesome.css" rel="stylesheet">
-<link href="../css/brands.css" rel="stylesheet">
-<link href="../css/solid.css" rel="stylesheet">
-</head>
-<body class="banner">
-<header class="header">
-<h3 class="logo blink">ExpTech.tw | 探索科技</h3>
-<a href="?action=login" class="ahref"><button class="button" style="vertical-align:middle"><span><i class="fab fa-discord"></i>'.Decoder(1).'</span></button></a>
+        echo '<header class="header">
+<h3 class="logo logotext">ExpTech.tw | 探索科技</h3>
+<a href="?action=login" class="ahref" target="_self"><button class="button" style="vertical-align:middle"><span><i class="fab fa-discord"></i>'.Decoder(1).'</span></button></a>
 </header>
-</body>
-</html>';
+<div class="loginfrom">
+<p class="ptext"><i class="fas fa-exclamation-triangle"></i>注意:</p>
+<p class="ptext">網站目前為測試版!</p>
+<p class="ptext">有問題請盡速回報!</p>
+</div>';
     }
 
     if(get('action') == 'login') {
@@ -312,10 +95,11 @@ function Decoder($Data){
     } 
     for ($i=0; $i < count($GLOBALS["Decoder"]); $i++) { 
         if($GLOBALS["Decoder"][$i]["id"]==$Data){
-            return $GLOBALS["Decoder"][$i][strtok($_SERVER['HTTP_ACCEPT_LANGUAGE'], ',')];
+            return $GLOBALS["Decoder"][$i][$GLOBALS["Language"]];
         }
     }
     return null;
 }
-
 ?>
+</body>
+</html>
